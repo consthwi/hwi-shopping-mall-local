f23051f6a059c2aa02cc4cebfb01e297f6fb7390
Hwiwon Kang <syaron9364@gmail.com>
2024-10-24 14:52:43 +0900

    local-fe: first commit

--- hwi-shopping-mall-be/app.js
+++ hwi-shopping-mall-be/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const indexRouter = require("./routes/index");
 
 // 기본 시동
 const app = express();
@@ -11,8 +12,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // req.body 객체로 인식하게 함
 
+// api 라우팅
+app.use("/api", indexRouter);
+
 // 데이터베이스 세팅
-const mongoURI = process.env.LOCAL_DB_ADDRESS;
+const mongoURI = process.env.MONGODB_URI_PROD;
+console.log(`mogoURI: ${process.env.MONGODB_URI_PROD}`);
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => console.log("mongoose connected"))
---
+++ hwi-shopping-mall-be/controllers/user.controller.js
@@ -0,0 +1,30 @@
+const userController = {};
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+userController.createUser = async (req, res) => {
+  try {
+    let { email, password, name, level } = req.body; // password 재할당 위해 let사용
+    // 1. 중복 메일 확인
+    const user = await User.findOne({ email: email });
+    if (user) {
+      throw new Error("가입한 이메일이 이미 존재합니다.");
+    }
+    // 2. password 암호화
+    const salt = await bcrypt.genSaltSync(10);
+    password = await bcrypt.hash(password, salt);
+    // 3. User모델로 newUser 생성 및 데이터베이스 저장
+    const newUser = new User({
+      email,
+      password,
+      name,
+      level: level ? level : "customer",
+    });
+    await newUser.save();
+    return res.status(200).json({ status: "ok" });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
+module.exports = userController;
